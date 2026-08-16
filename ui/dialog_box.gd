7ca613aef87aa192472938014bff202fc337a938
aautarch <k.nitta.author@gmail.com>
2026-08-16 20:38:33 +0800

    📃: ui: hud_layer updated with niew children
    - text_box.gd
    - hud_layer.gd modified to support states
    - dialog_box creaeted

---
+++ ui/dialog_box.gd
@@ -0,0 +1,5 @@
+class_name DialogBox
+extends Panel
+
+@onready var richTextLabel : RichTextLabel = $bodyLabel
+@onready var headerTextLabel : Label = $headerLabel
--- ui/hud_layer.gd
+++ ui/hud_layer.gd
@@ -1,4 +1,25 @@
+class_name HudLayer
 extends CanvasLayer
 
-@onready var playerHpBar = $PlayerHpBar
-@onready var enemyHpBar = $EnemyHpBar
+enum STATE {
+    RESUMED,
+    PAUSED,
+    SETTINGS,
+}
+
+@export var currentState: STATE:
+    set(value):
+        currentState = value
+
+        match currentState:
+            STATE.RESUMED:
+                pass
+            STATE.PAUSED:
+                pass
+            STATE.SETTINGS:
+                pass
+
+@onready var playerHpBar : ProgressBar = $Control/PlayerHpBar
+@onready var enemyHpBar : ProgressBar = $Control/EnemyHpBar
+@onready var dialogBox : DialogBox = $Control/DialogBox
+@onready var textBox: TextBox = $Control/TextBox
