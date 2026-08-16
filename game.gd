959b068cf312d148cb13f12503c37fbd77c6fc02
aautarch <k.nitta.author@gmail.com>
2026-08-16 20:49:09 +0800

    🎮: game: scene set up for level loading
    - game scene set up
    - game.gd set up to load level

--- game.gd
+++ game.gd
@@ -4,13 +4,22 @@ extends Node2D
 const GRID_SIZE = 16 # potentially subject to change
 const GRAVITY = 1 # determine best value later on
 
-@onready var tileMapLayer = $TileMapLayer
 @onready var hudLayer = $HudLayer
+@onready var music: AudioStreamPlayer = $Music
+@onready var stage: Stage = $Stage
 
-@export var currentStageFilePath : String
+@export_file("*.tscn") var currentStageFilePath : String
+
+func start_stage():
+	stage = load_stage(currentStageFilePath)
+	add_child(stage)
 
 func load_stage(file_path: String) -> Stage:
-    return null
 
-func unload_stage(current_stage: Stage) -> void:
-    pass
+	var stageFile : PackedScene = load(file_path)
+
+	return stageFile.instantiate()
+
+func unload_stage() -> void:
+	stage.call_deferred("queue_free")
+	stage = null
