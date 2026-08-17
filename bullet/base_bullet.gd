e9f39115de17fce5b477629d6a26b7265043bc16
aautarch <k.nitta.author@gmail.com>
2026-08-17 15:49:40 +0800

    minor commit

--- bullet/base_bullet.gd
+++ bullet/base_bullet.gd
@@ -3,5 +3,17 @@ extends Area2D
 
 @onready var sprite2D = $Sprite2D
 
-func Update():
-    pass
+@export var movement_angle : int:
+    set(value):
+        movement_angle = value
+        velocity = Vector2.UP.rotated(movement_angle) * speed
+
+@export var speed : Vector2:
+    set(value):
+        speed = value
+        velocity = Vector2.UP.rotated(movement_angle) * speed
+        
+@export var velocity : Vector2
+
+func Update(delta):
+    global_position += velocity * delta 
