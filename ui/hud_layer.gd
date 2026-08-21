0b616aa412b8d45d4ae9a1ddb95c3d19cf556bf4
aautarch <k.nitta.author@gmail.com>
2026-08-21 10:25:36 +0800

    🎮,🖥️:game, ui: start_screen and credits_screen
    - start_screen created
    - credits_screen created
    - hud_layer now set up for
    - pause_screen set up in hud_layer
    - pause_button set up

--- ui/hud_layer.gd
+++ ui/hud_layer.gd
@@ -26,3 +26,11 @@ enum STATE {
 @onready var textBox: TextBox = $Control/TextBox
 @onready var anim: AnimationPlayer = $anim
 @onready var autoSaveIcon: TextureRect = $Control/autoSaveIcon
+
+func _unhandled_input(event: InputEvent) -> void:
+
+    if event.is_action_pressed("pause"):
+        if currentState != STATE.PAUSED:
+            currentState = STATE.PAUSED
+        else:
+            currentState = STATE.RESUMED
