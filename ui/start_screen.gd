cab71435cf862c64c6097d0af204deb349de5081
aautarch <k.nitta.author@gmail.com>
2026-08-25 07:03:39 +0800

    🖥️:hud: various screens created
    - opening_screen created
    - settings_screen created
    - win_screen created
    - battle_control created
    - start_screen modified

--- ui/start_screen.gd
+++ ui/start_screen.gd
@@ -1,10 +1,23 @@
 extends Control
 
+signal start_level(from_beginning)
 
-func start_game() -> void: pass
+@onready var startButton := $startButton
+@onready var loadButton := $continueButton
+@onready var settingsButton := $settingsButton
+@onready var exitButton := $exitButton
 
-func load_game() -> void: pass
+func _ready() -> void:
+
+    startButton.connect("pressed", start_game)
+    loadButton.connect("pressed", load_game)
+    settingsButton.connect("pressed", settings_menu)
+    exitButton.connect("pressed", exit_game)
+
+func start_game() -> void: emit_signal("start_level", false)
+
+func load_game() -> void: emit_signal("start_level", true)
 
 func settings_menu() -> void: pass
 
-func exit_game() -> void: pass
+func exit_game() -> void: get_tree().quit()
