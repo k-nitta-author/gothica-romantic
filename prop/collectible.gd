2d5bfb38b93345be230206ff4adaf0ef916cc6d7
aautarch <k.nitta.author@gmail.com>
2026-08-26 10:35:15 +0800

    🪑:prop: bullet_prop created and set up
    - bullet prop created and set up
    - player bulletsCurrent setter modified to clamp value between 0 and max

--- prop/collectible.gd
+++ prop/collectible.gd
@@ -3,8 +3,13 @@ extends BaseProp
 
 signal collected(collectible: CollectibleProp)
 
+
+func collect() -> void:
+	emit_signal("collected", self)
+
 func OnAreaEntered(area: Area2D) -> void:
 	super(area)
 
+	collect()
 	hitPoints -= 1
-	emit_signal("collected", self)
+
