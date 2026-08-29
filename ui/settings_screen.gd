033aa9f379e684ece86b9ceb3a4766aad2b0b3af
aautarch <k.nitta.author@gmail.com>
2026-08-29 13:20:33 +0800

    🖥️:ui: setings screen added to start screen
    - settings screen created and set up
    - start screen able to display settings screen

--- ui/settings_screen.gd
+++ ui/settings_screen.gd
@@ -1,2 +1,71 @@
 extends Control
 
+# initialize all variables
+@onready var volumeSlider : HSlider = $volumeSlider
+@onready var masterVolumeSlider : HSlider = $masterVolumeSlider
+@onready var musicVolumeSlider : HSlider = $musicVolumeSlider
+@onready var backButton : Button = $backButton
+
+const VOLUME_SETTINGS_PATH := "user://audio_settings.json"
+
+signal return_to_previous_screen # emit to remove from view
+
+func _ready() -> void:
+    
+    # load all settings from file
+    load_from_file()
+    
+    # connect each slider to its handler
+    volumeSlider.connect("value_changed", on_volume_slider_value_changed)
+    masterVolumeSlider.connect("value_changed", on_master_volume_slider_value_changed)
+    musicVolumeSlider.connect("value_changed", on_music_slider_value_changed)
+
+    # connect the back button 
+    backButton.connect("pressed", on_back_button_pressed)
+
+# when the back button is pressed
+func on_back_button_pressed() -> void: emit_signal("return_to_previous_screen")
+
+# called when the volume slider is changed
+func on_volume_slider_value_changed(_value: float) -> void: save_to_file(get_data_dict())
+
+# called when the music slider is changed
+func on_music_slider_value_changed(_value: float) -> void: save_to_file(get_data_dict())
+
+# called when the master volume slider is changed
+func on_master_volume_slider_value_changed(_value: float) -> void: save_to_file(get_data_dict())
+
+# load the volume settings from the file
+func load_from_file() -> void:
+
+    # first check if file exists at all; go away if not
+    if !FileAccess.file_exists(VOLUME_SETTINGS_PATH): return
+
+    var file := FileAccess.open(VOLUME_SETTINGS_PATH, FileAccess.READ)
+
+    var parsed_json : Dictionary = JSON.parse_string(file.get_as_text())
+
+    file.close()
+
+    volumeSlider.value = parsed_json["volumeSlider"]
+    musicVolumeSlider.value = parsed_json["musicVolumeSlider"]
+    masterVolumeSlider.value = parsed_json["master_volume"]
+
+# poll all sliders for their current value
+func get_data_dict() -> Dictionary:
+    return {
+        "master_volume": masterVolumeSlider.value,
+        "musicVolumeSlider": musicVolumeSlider.value,
+        "volumeSlider": volumeSlider.value
+    }
+
+# save settings to file
+func save_to_file(data: Dictionary) -> void:
+    var file := FileAccess.open(VOLUME_SETTINGS_PATH, FileAccess.WRITE)
+
+    var json = JSON.stringify(data)
+
+    file.store_string(json)
+
+    file.close()
+
--- ui/start_screen.gd
+++ ui/start_screen.gd
@@ -1,14 +1,21 @@
 extends Control
 
+# the start level signal
 signal start_level(from_beginning)
 
+# intiialize each button
 @onready var startButton := $startButton
 @onready var loadButton := $continueButton
 @onready var settingsButton := $settingsButton
 @onready var exitButton := $exitButton
 
+# the save game modal
 var save_game_modal
 var save_game_modal_scene : PackedScene = preload("uid://dy0j84bmvhox2")
+
+# the settigns modal 
+var settings_modal
+var settings_modal_scene : PackedScene = preload("uid://dkxidum8tt7pr")
 
 func _ready() -> void:
 	startButton.connect("pressed", start_game)
@@ -16,20 +23,36 @@ func _ready() -> void:
 	settingsButton.connect("pressed", settings_menu)
 	exitButton.connect("pressed", exit_game)
 
+# show the save game modal
 func show_save_game_modal() -> void:
 	save_game_modal = save_game_modal_scene.instantiate()
 	save_game_modal.connect("return_to_previous_screen", on_quit_save_game_modal)
 	add_child(save_game_modal)
 
+# remove the save game modal
 func on_quit_save_game_modal() -> void:
 	hide_save_game_modal()
 
+# show the settings modal
+func show_settings_modal() -> void:
+	settings_modal = settings_modal_scene.instantiate()
+	settings_modal.connect("return_to_previous_screen", on_quit_settings_game_modal)
+	add_child(settings_modal)
+
+# called when the settings modal is quit
+func on_quit_settings_game_modal() -> void: settings_modal.queue_free()
+
+# called when the save modal is quit
 func hide_save_game_modal() -> void: save_game_modal.queue_free()
 
+# called when the player clicks the start button
 func start_game() -> void: emit_signal("start_level", false)
 
+# called when the load game button
 func load_game() -> void: emit_signal("start_level", true)
 
-func settings_menu() -> void: pass
+# called when the settings button is clicked
+func settings_menu() -> void: show_settings_modal()
 
+# called when the exit button is clicked
 func exit_game() -> void: get_tree().quit()
