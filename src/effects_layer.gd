6f885f7a17567756366d55110691d0e1d7bfd099
aautarch <k.nitta.author@gmail.com>
2026-09-03 10:33:06 +0800

    effects_layer updated

--- src/effects_layer.gd
+++ src/effects_layer.gd
@@ -3,3 +3,28 @@ extends CanvasLayer
 @onready var transitionSurface : ColorRect = $transitionSurface
 @onready var anim : AnimationPlayer = $anim
 
+# the different trans types
+enum TRANS {
+    WIPE_LEFT,
+    WIPE_RIGHT,
+    WIPE_UP,
+    WIPE_DOWN,
+    IRIS_OUT,
+    IRIS_IN
+}
+
+# plays the transition for the effects layer
+func play_transition(trans_type: TRANS) -> void: 
+
+    # map chosen TRANS type with corresponding animation
+    const anim_dict = {
+        TRANS.WIPE_LEFT: "side_wipe_left",
+        TRANS.WIPE_RIGHT: "side_wipe_right",
+        TRANS.WIPE_UP: "side_wipe_up",
+        TRANS.WIPE_DOWN: "side_wipe_down",
+        TRANS.IRIS_IN: "iris_in",
+        TRANS.IRIS_OUT: "iris_out"
+        }
+
+    # play animation itself
+    anim.play(anim_dict[trans_type])
