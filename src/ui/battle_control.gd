330a75ff151c764d8539f3f23504adb769178d0f
aautarch <k.nitta.author@gmail.com>
2026-09-06 19:23:53 +0800

    🖥️:hud: battle_control made into seprate scene

--- src/ui/battle_control.gd
+++ src/ui/battle_control.gd
@@ -4,6 +4,8 @@ extends Control
 @onready var BossHpBarLabel = $"BossHpBar/small label2"
 @onready var BossHpProgressBar = $EnemyHpBar
 
+@onready var bulletBar: Array[Node] = [ $BulletIcon, $BulletIcon2, $BulletIcon3, $BulletIcon4, $BulletIcon5, $BulletIcon6]
+
 func bind_boss_hp_bar(boss: BaseActor) -> void:
 	BossHpProgressBar.max_value = boss.max_hp
 	BossHpProgressBar.value = boss.current_hp
@@ -18,3 +20,13 @@ func toggle_boss_hp_bar_visible() -> void:
 func update_boss_hp_bar(_actor: BaseActor, _old_hp: float, new_hp: float) -> void:
 	BossHpProgressBar.value = new_hp
 	BossHpBar.visible = new_hp != 0
+
+func update_bullet_bar(_old_value: int, bulletsCurrent: int):
+
+	var bulletBarIdx := bulletsCurrent - 1
+
+	bulletBar[bulletBarIdx].visible = true
+
+	for i in range(6):
+
+		bulletBar[i].visible = i < bulletsCurrent
--- src/ui/hud_layer.gd
+++ src/ui/hud_layer.gd
@@ -33,14 +33,6 @@ enum STATE {
 @onready var dialogBox : DialogBox = $Control/DialogBox
 @onready var startScreen = $Control/StartScreen
 
-@onready var bulletBar: Array[Node] = [
-	$Control/BattleControl/BulletIcon,
-	$Control/BattleControl/BulletIcon2,
-	$Control/BattleControl/BulletIcon3,
-	$Control/BattleControl/BulletIcon4,
-	$Control/BattleControl/BulletIcon5,
-	$Control/BattleControl/BulletIcon6]
-
 @onready var textBox: TextBox = $Control/TextBox
 @onready var anim: AnimationPlayer = $anim
 @onready var autoSaveIcon: TextureRect = $Control/autoSaveIcon
@@ -78,26 +70,15 @@ func on_game_paused() -> void:
 
 func bind_to_player(p: Player):
 	p.connect("has_hp_changed", update_hp_bar)
-	p.connect("on_bullets_current_change", update_bullet_bar)
+	p.connect("on_bullets_current_change", battleControl.update_bullet_bar)
 	p.connect("on_potions_current_change", $Control/BattleControl/potionIcons.update_potion_icons)
 
 func bind_boss(bosses: Array): for b in bosses: battleControl.bind_boss_hp_bar(b)
 
 func get_start_screen() -> Control: return $Control/StartScreen
 
-func update_bullet_bar(_old_value: int, bulletsCurrent: int):
-
-	var bulletBarIdx := bulletsCurrent - 1
-
-	bulletBar[bulletBarIdx].visible = true
-
-	for i in range(6):
-
-		bulletBar[i].visible = i < bulletsCurrent
-
 func update_hp_bar(_actor: BaseActor, _old_value: int, current_hp: int):
 	playerHpBar.value = current_hp
-
 
 func _unhandled_input(event: InputEvent) -> void:
 
