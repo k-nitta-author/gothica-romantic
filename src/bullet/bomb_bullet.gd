9a90cb65e7dd7a631fcae2b89c4d073df2b84b40
aautarch <k.nitta.author@gmail.com>
2026-09-12 10:02:07 +0800

    🔫:bullet: create bomb_bullet
    - director_bomb created and configured as example of explosive

--- src/bullet/bomb_bullet.gd
+++ src/bullet/bomb_bullet.gd
@@ -4,13 +4,24 @@ extends BaseBullet
 @onready var anim: AnimationPlayer = $anim
 @onready var explosionArea: Area2D = $explosionArea
 
+func _ready() -> void:
+    super()
+    anim.connect("animation_finished", on_animation_finished)
+
+# when the animation is finished, the bomb must disappear
+func on_animation_finished(anim_name: String) -> void: if anim_name == "explode": isInactive = true
+
 # override and connect explode signal to stage
 func bind_dependencies(stage: Stage):
     connect("explode", stage.effectsManager.spawn_effects)
 
+# the explode method
+func explode() -> void:
+    velocity = Vector2.ZERO
+    anim.play("explode")
+
 # override and play an explosion which is able to damage player
-func on_area_entered(area: Area2D) -> void:
-    if area.owner is BaseActor:
-        isInactive = true
-        velocity = Vector2.ZERO
-        anim.play("explode")
+func on_body_entered(body: Node2D) -> void: explode()
+
+# override and play an explosion which is able to damage player
+func on_area_entered(area: Area2D) -> void: explode()
