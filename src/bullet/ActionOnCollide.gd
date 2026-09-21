32cc051b1289812f8dc698ba40e7c2641a80fa70
aautarch <k.nitta.author@gmail.com>
2026-09-21 10:24:57 +0800

    simplified actionOnCollide

--- src/bullet/ActionOnCollide.gd
+++ src/bullet/ActionOnCollide.gd
@@ -3,15 +3,15 @@ extends Resource
 
 
 # sets up the bullet to perform the collision
-func setup(b: BaseBullet, s: Stage) -> void: pass
+func setup(b: BaseBullet, s: Stage) -> void:
+	b.connect("notify_attack_connection", s.effectsManager.spawn_effects)
+
+func act(b: BaseBullet, collision_point: Vector2) -> void:
+	b.emit_signal("notify_attack_connection", collision_point, b.velocity.y < 0, EffectsManager.SPLATTER.SHOOT)
+	b.isInactive = true
 
 # meant to be overriden by child classes
-func act_on(b: BaseBullet, area: Area2D) -> void:
-    var collision_point := b.global_position
-    b.emit_signal("notify_attack_connection", collision_point, b.velocity.y < 0, Stage.SPLATTER.SHOOT)
-    b.isInactive = true
+func act_on(b: BaseBullet, area: Area2D) -> void: act(b, b.global_position)
 
-func act_on_body(b: BaseBullet, body: Node2D) -> void:
-    var collision_point := b.global_position
-    b.emit_signal("notify_attack_connection", collision_point, b.velocity.y < 0, Stage.SPLATTER.SHOOT)
-    b.isInactive = true
+# meant to be overridden by child classes
+func act_on_body(b: BaseBullet, body: Node2D) -> void: act(b, b.global_position)
--- src/bullet/action_on_collide/ActionDisappear.gd
+++ src/bullet/action_on_collide/ActionDisappear.gd
@@ -1,4 +1,2 @@
 class_name ActionDisappear
 extends ActionOnCollide
-
-func act_on(b: BaseBullet, area: Area2D) -> void: super(b, area)
