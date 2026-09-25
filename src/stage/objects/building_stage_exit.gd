55f4e0b56829cc62127e9c07fa975a388496d89a
aautarch <k.nitta.author@gmail.com>
2026-09-25 16:41:46 +0800

    update stage_exit
    - add is_usable to base stage_exit to control use of stage_exit

--- src/stage/objects/building_stage_exit.gd
+++ src/stage/objects/building_stage_exit.gd
@@ -9,20 +9,32 @@ func _ready() -> void:
     connect("body_exited", on_body_exited)
 
 func on_body_exited(_body: Node2D) -> void:
+
+    if !is_usable: return
+
     is_player_interactible = false
     toolTip.visible = false
 
 # in the case of the base_stage exit, immediately change stage
 func on_body_entered(_body: Node2D) -> void:
+    if !is_usable: return
+    
     is_player_interactible = true
     toolTip.visible = true
 
+func _physics_process(_delta) -> void:
+    if (toolTip.visible == false and !is_usable and is_player_interactible):
+        toolTip.visible = true
+    else:
+        return
+
 func _unhandled_input(event: InputEvent) -> void:
 
-    if !is_player_interactible: return
+    if !is_player_interactible or !is_usable: return
 
     if event.is_action_pressed("enterBuilding"): notify_stage_change()
 
 # the stage change method
 func notify_stage_change() -> void:
+
     emit_signal("player_exited", NextLevel, Stage.EXIT_TYPE.TO_INTERIOR, egress_idx)
--- src/stage/objects/stage_exit.gd
+++ src/stage/objects/stage_exit.gd
@@ -1,10 +1,18 @@
 class_name StageExit
 extends Area2D
 
-@export var NextLevel : PackedScene  
+@export var is_usable: bool = true
+
+@export var NextLevel : PackedScene
 @export var egress_idx: int
 
 signal player_exited(nextLevel, exit_type, _egress)
+
+func toggle_is_usable() -> bool:
+    
+    is_usable = !is_usable
+
+    return is_usable 
 
 func bind_to_stage(stage: Stage) -> void:
 
@@ -16,4 +24,8 @@ func _ready() -> void: connect("body_entered", on_body_entered)
 func on_body_entered(body: Node2D) -> void: notify_stage_change()
 
 # the stage change method
-func notify_stage_change() -> void: emit_signal("player_exited", NextLevel, Stage.EXIT_TYPE.TO_NEXT_STAGE, egress_idx)
+func notify_stage_change() -> void:
+
+    if !is_usable: return 
+
+    emit_signal("player_exited", NextLevel, Stage.EXIT_TYPE.TO_NEXT_STAGE, egress_idx)
